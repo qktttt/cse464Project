e2e432b581a445fdce9f3958e6d1ba01e68f8ddf
Kunting Qi <qik@miamioh.edu>
2018-11-18 19:39:55 -0500

    add class Vertice and class SPCity

---
+++ src/SPCity.java
@@ -0,0 +1,12 @@
+public class SPCity {
+	
+	public SPCity() {
+
+	}
+
+	public void LoadMap(String dataFileName) {
+
+	}
+
+	public 
+}
---
+++ src/Vertice.java
@@ -0,0 +1,5 @@
+public class Vertice {
+	public Vertice() {
+
+	}
+}
